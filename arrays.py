b435827c1d7ca42257b37276363ffce14bda60ab
mstephenp <mpalmer1661@gmail.com>
2015-04-15 22:19:08 -0500

    movement changes

--- arrays.py
+++ arrays.py
@@ -1,22 +1,24 @@
 from random import randint
 
-world   = {}
-width   = 90
-height  = 45
+world = {}
+width = 65
+height = 26
 plt_nrg = 80
 rep_nrg = 200
 
-def draw_world(world):
+
+def draw_world(w):
     for y in range(height):
         print("|", end="")
         for x in range(width):
-            if world[(x,y)][0]:
+            if w[(x, y)][0]:
                 print("M", end="")
-            elif world[(x,y)][1]:
+            elif w[(x, y)][1]:
                 print("*", end="")
             else:
                 print(" ", end="")
         print("|\n", end="")
+
 
 def simulate_day():
     add_plants()
@@ -28,7 +30,8 @@ def simulate_day():
             reproduce_animal(key)
             check_for_dead(key)
             move_animal(key)
-            
+
+
 def organism_count():
     a_count = 0
     p_count = 0
@@ -38,69 +41,102 @@ def organism_count():
         if value[1]:
             p_count += 1
     print("Animals: {}, Plants: {}".format(a_count, p_count))
-        
+
+
 def move_animal(key):
-    
-    # Add logic for deciding which way to move
-    x,y = (key[0] + 1) % width, (key[1] + 1) % height
+
+    if 2 <= world[key][3] < 5:
+            factor_x = 1
+    elif world[key][3] == 1 or world[key][3] == 5:
+        factor_x = 0
+    else:
+        factor_x = -1
+
+    if 0 <= world[key][3] < 3:
+        factor_y = -1
+    elif world[key][3] >= 4 or world[key][3] < 7:
+        factor_y = 1
+    else:
+        factor_y = 0
+
+    x, y = (key[0] + factor_x) % width, (key[1] + factor_y) % height
     
     moved = world[key].copy()
     
-    moved[1] = world[x,y][1]
+    moved[1] = world[x, y][1]
     moved[2] -= 1
-    world[(x,y)] = moved
+    world[(x, y)] = moved
     
-    world[key][0] =  False
+    world[key][0] = False
+
 
 def turn_animal(key):
     
     # Add logic to decide how to turn
     world[key][3] = (world[key][3] + 1) % 8
 
+
 def eat_animal(key):
     
     if world[key][0] and world[key][1]:
         world[key][2] += plt_nrg
         world[key][1] = False
-        
+
+
 def reproduce_animal(key):
     
     if world[key][2] > rep_nrg:
         world[key][2] = int(world[key][2] / 2)
         copy_animal(key)
-        
+
+
+def mutate_genes(genes):
+
+    largest = max(genes)
+    total = sum(genes)
+    index = genes.index(largest)
+
+    genes[index] = (genes[index] + total)
+
+    return genes
+
+
 def copy_animal(key):
     
-    #just like the move function
-    x,y = (key[0] - randint(1,4)) % width, (key[1] - randint(1,4)) % height
+    # just like the move function
+    x, y = (key[0] - randint(1, 4)) % width, (key[1] - randint(1, 4)) % height
     
     moved = world[key].copy()
     
-    moved[1] = world[x,y][1]
+    moved[1] = world[x, y][1]
     moved[2] -= 1
-    world[(x,y)] = moved
+    moved[4] = mutate_genes(moved[4])
+    world[(x, y)] = moved
+
 
 def check_for_dead(key):
     if world[key][2] < 1:
         world[key][0] = False
-        
+
+
 def add_plants():
     
     x, y = randint(1, width - 1), randint(1, height - 1)
     j_x, j_y = randint(int(width / 3), int(width / 2)), randint(int(height / 3), int(height / 2))
     
-    world[(x,y)][1] = True
-    world[(j_x,j_y)][1] = True
+    world[(x, y)][1] = True
+    world[(j_x, j_y)][1] = True
 
-def main(argv=None):
+
+def main():
     
     # Randomly generate a set of genes for the first animal
-    genes = [randint(1,8) for i in range(8)]
+    genes = [randint(1, 8) for _ in range(8)]
     
     # Initialize the whole world
     for x in range(width):
         for y in range(height):
-            world[(x,y)] = [False, False, 1000, 0, genes]
+            world[(x, y)] = [False, False, 1000, 0, genes]
     
     # Add the first animal - right in the middle
     world[(int(width / 2), int(height / 2))][0] = True
@@ -111,6 +147,8 @@ def main(argv=None):
         num = input('Days:')
         day_count += int(num)
         for i in range(int(num)):
+            if i % 1000 == 0:
+                print('*')
             simulate_day()
             
         print("Days: {} Years: {}".format(day_count, int(day_count / 365)))
